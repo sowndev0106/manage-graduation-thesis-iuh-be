57ede1e1e690414c4dcda981e0588144db91d4ad
son.nguyenthanh <=>
2023-05-29 14:52:12 +0700

    fix bug

--- src/lecturer/application/handlers/student/GetListStudent.ts
+++ src/lecturer/application/handlers/student/GetListStudent.ts
@@ -51,7 +51,7 @@ export default class GetListStudent extends RequestHandler {
         true
       );
       const studentExistsTopicJson = studentExistsTopic?.map((e) => {
-        return { ...e.toJSON, isTopicExists: false };
+        return { ...e.toJSON, isTopicExists: true };
       });
       const studentNotExistsTopic = await this.studentDao.findAll(
         input.termId,
@@ -63,7 +63,7 @@ export default class GetListStudent extends RequestHandler {
       studentsJson = [...studentExistsTopicJson, ...studentNotExistsTopicJson];
       studentsJson = studentsJson.sort(
         (a: any, b: any) =>
-          new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+          new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime()
       );
     }
     return studentsJson;
